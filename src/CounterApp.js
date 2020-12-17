f60cde7d497f94610a54b8c3181ff70007e4054f
pipopotamasu <ec0156hx@gmail.com>
2020-12-17 14:57:47 +0900

    counter app state

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,13 +1,43 @@
+import { useReducer } from 'react';
+import { createSlice } from '@reduxjs/toolkit';
 import './CounterApp.css';
 
+const initialState = { counter: 0 };
+
+const slice = createSlice({
+  name: 'counter',
+  reducers:{
+    INCREMENT: (state) => {
+      state.counter += 1;
+    },
+    DECREMENT: (state) => {
+      state.counter -= 1;
+    },
+    RESET: (state) => {
+      state.counter = 0;
+    },
+  },
+  initialState
+});
+
+const { actions, reducer } = slice;
+
 function CounterApp() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="counter-app">
-      <p className="counter">Counter: 0</p>
+      <p className="counter">Counter: { state.counter }</p>
       <ul className="buttons">
-        <li><button>+</button></li>
-        <li><button>-</button></li>
-        <li><button>reset</button></li>
+        <li>
+          <button onClick={ () => dispatch(actions.INCREMENT) }>+</button>
+        </li>
+        <li>
+          <button onClick={ () => dispatch(actions.DECREMENT) }>-</button>
+        </li>
+        <li>
+          <button onClick={ () => dispatch(actions.RESET) }>reset</button>
+        </li>
       </ul>
     </div>
   );
