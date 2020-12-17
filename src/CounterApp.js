3cc90c420541a6d34f0f0970a29f545c6f9258d6
pipopotamasu <ec0156hx@gmail.com>
2020-12-17 14:09:40 +0900

    rename App -> CounterApp

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,4 +1,4 @@
-function App() {
+function CounterApp() {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,4 +18,4 @@ function App() {
   );
 }
 
-export default App;
+export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CounterApp from './CounterApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
